82832a17d1898e6a017c390428c24f9a7d72fd6a
Roger David Cruz <roger.david.cruz@gmail.com>
2019-07-15 09:28:03 -0400

    nasa simple api js

--- main.js
+++ main.js
@@ -1,43 +1,34 @@
-// Nasa API - enter date-get picture of date
-// Get Your API Key
-// Your API key for johannfolta@gmail.com is:
-//
-// gPBTc1nbfvN3RBbEacc0yFCVALuTdb1itJcp1PQX
-// You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:
+// Simple NASA API - Picture of the day
 
-// https://api.nasa.gov/planetary/apod?api_key=gPBTc1nbfvN3RBbEacc0yFCVALuTdb1itJcp1PQX
-// Account Email: johannfolta@gmail.com
-// Account ID: 292fe41f-52d5-464a-b1ac-1b93aa1db4bd
+// Account Email: blueduckco123@gmail.com
+// API Key: r848LTb9LBZUF2aIjK0GtNrX8C3aLmtAsKaq6X73
+// API URL: https://api.nasa.gov/planetary/apod?api_key=r848LTb9LBZUF2aIjK0GtNrX8C3aLmtAsKaq6X73
 
 // QUERY PARAMETERS
 // Parameter	Type	Default	Description
-// date	YYYY-MM-DD	today	The date of the APOD image to retrieve
+// date	YYYY-MM-DD of the NASA POD image to be retrieve
 // add "&date=2019-07-05" to URL
 
-// fetch("https://api.nasa.gov/planetary/apod?api_key=gPBTc1nbfvN3RBbEacc0yFCVALuTdb1itJcp1PQX")
-//     .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-//     .then(response => {
-//         console.log(response.drinks[0].strDrink)
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//         alert("sorry, there are no results for your search")
-//     });
 document.querySelector("button").addEventListener("click", function(){
 
-// ask leon about .textContent?
   const date = document.querySelector("input").value
-  // document.querySelector("h1").innerHTML = "hello"
 
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=gPBTc1nbfvN3RBbEacc0yFCVALuTdb1itJcp1PQX&date=${date}`)
-  .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=r848LTb9LBZUF2aIjK0GtNrX8C3aLmtAsKaq6X73&date=${date}`)
+  .then(res => res.json())
   .then(response => {
     console.log(response)
-    document.querySelector("img").src= response.hdurl
+    if(response.media_type === "image"){
+        document.querySelector("img").src= response.hdurl
+        document.querySelector("iframe").style.display = "none"
+        document.querySelector("img").style.display = "initial"
+    }else{
+        document.querySelector("iframe").src= response.url
+        document.querySelector("img").style.display = "none"
+        document.querySelector("iframe").style.display = "initial"
+    }
   })
   .catch(err => {
     console.log(`error ${err}`)
     alert("sorry, there are no results for your search")
   });
-
 })
